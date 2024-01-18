d9bb6e8d216c3f94487dea8c4939fa1b8fc409f4
Heizenberg <popov.roman91@gmail.com>
2024-01-18 16:42:53 +0200

    added login page and user signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,20 @@
+const User = require("../models/user.model");
+
 function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-function signup(req, res) {
-  
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postalcode,
+    req.body.city
+  );
+  await user.signup();
+  res.redirect("/login");
 }
 
 function getLogin(req, res) {
