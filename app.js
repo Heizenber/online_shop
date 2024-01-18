42f1c68a3ddc4ea894140418bfdf2d2bdf184ad5
Heizenberg <popov.roman91@gmail.com>
2024-01-18 15:31:36 +0200

    added connection to database verification in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('./data/database');
 
 const authRoutes = require('./routes/auth.routes');
 
@@ -12,4 +13,10 @@ app.use(express.static('public'));
 
 app.use(authRoutes);
 
-app.listen(3000);
+
+db.connectToDatabase().then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log("Failed to connect to database!");
+    console.log(err);
+});
